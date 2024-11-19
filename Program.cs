dcdbd16fbf6baf32fbd7b58a374c88c09cd7ec90
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-11-19 09:28:35 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp_10
+{
+    class Program
+    {
+        static void gg()
+        {
+            Console.WriteLine(((Math.Sqrt(5) + 5) / 2) + ((Math.Sqrt(12) + 12) / 2) + ((Math.Sqrt(19) + 19) / 2));
+        }
+
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args">Строковый массив аргументов командной строки</param>
+        static void Main(string[] args)
+        {
+            // Call gg() method
+            gg();
+            Console.Read();
+        }
+    }
+}
